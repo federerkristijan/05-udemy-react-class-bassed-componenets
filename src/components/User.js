a1e03a9514e58e7b8dbfff606673b2fb6a9ccbd3
Kristijan Federer <federer.kristijan@gmail.com>
2022-06-28 11:11:48 +0200

    added class based components, moved .eslintcache to node_modules/.cache

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,15 @@
+import { Component } from 'react';
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+// class component need import Component and pas on props with 'this.'
+class User {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
